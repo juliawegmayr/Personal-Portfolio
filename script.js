efc29a8a5d969f51dd06622032f86b14d339a079
juliawegmayr <kontakt@juliawegmayr.at>
2023-01-11 15:49:27 +0100

    add mobile navigation

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const burger = document.querySelector("#burger-menu");
+const ol = document.querySelector("nav ol");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ol.classList.toggle("show");
+  });
+
+  // Close hamburger menu when a link is clicked
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ol.classList.remove("show");
+  })
+);
